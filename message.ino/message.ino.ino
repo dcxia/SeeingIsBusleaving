f7c225f6f85e43a1e3196b248cdadd949fcc3a21
Jacob Gordon <gordoj7@mcmaster.ca>
2018-02-17 18:15:37 -0500

    First attempt at a split function and implementing this into the message class to create a function for creating a message object

--- message.ino/message.ino.ino
+++ message.ino/message.ino.ino
@@ -1,7 +1,39 @@
 
+String[] splitSMS(String a )
+{
+  String b[4];
+  int subStart = 0;
+  int subArrayIndex = 0;
+  for ( int i = 0 ; i < a.length() i++) 
+  {
+    if (a[i]  == ",")
+    {
+      b[subArrayIndex] = a.subString(subStart,i-1);
+      subArrayIndex++;
+      subStart = i+1;
+    }
+    else if ( a[i] == "\n")
+    {
+      b[subArrayIndex] = a.subString(subStart,i-1);
+      return (b);
+    }
+  }
+}
+
+
 class message
 {
   public:
-        int dir; String ID; int startID; int stopID;
-}
+        bool dir; String ID; int startID; int stopID;
+  message smsConvert( String SMS)
+  {
+    String smsArray[4] = splitSMS(SMS);
+    if (smsArray[0] == "1") { dir = True; }
+    else { dir = false ; }
+    ID = smsArray[1];
+    startID = smsArray[2];
+    stopID = smsArray[3];
+    
+  }
+};
 
