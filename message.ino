3996cf4586a1e33a45997979452bd6292e5f325b
Jacob Gordon <gordoj7@mcmaster.ca>
2018-02-17 17:29:25 -0500

    Added message class

---
+++ message.ino
@@ -0,0 +1,24 @@
+
+class ( String[] SMS)
+{
+  private:
+    bit dir; String id; int start; int stop;
+  public:
+    bit getDir()
+    {
+      return dir;
+    }
+    String getId()
+    {
+      return id;
+    }
+    int getStart()
+    {
+      return start;
+    }
+    int getStop()
+    {
+      return stop;
+    }
+}
+
